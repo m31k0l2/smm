8ea2ca59f7f3bd37a2e5f2292b31ab1b9e3a5c75
m31k0l2 <m31k0l2@gmail.com>
2018-10-17 09:00:45 +0300

    v0

---
+++ src/pages/app.js
@@ -0,0 +1,6 @@
+import React from "react"
+import LSidebar from "../components/lsidebar"
+
+export default () => (
+  <LSidebar styleName="hideSidebar"/>
+)
--- src/pages/login.js
+++ src/pages/login.js
@@ -2,15 +2,16 @@ import React from "react"
 import styles from "./login.module.css"
 import { Link } from "gatsby"
 // import { GridContainer, GridItem } from "react-css-grid-layout"
+import { FaVk } from 'react-icons/fa';
 
 export default () => (
 
-<div className="wrapper">
+<div className={styles.wrapper}>
     <div className={styles.header}><h4>SMM-PUBLISHER: система управления постами в социальных сетях</h4></div>
     <div className={styles.content}>
         <h2>Войти</h2>
         <p>Пожалуйста, войдите с одним из ваших социальных аккаунтов.</p>
-        <a href="#" className={styles.button} data-vk>Войти в ВКонтакте</a>
+        <Link to="/app/" className={styles.vk} data-vk><i className={styles.vkIcon}><FaVk/></i><span>Войти с ВКонтакте</span></Link>
     </div>
     <div className={styles.footer}><h4>m31k0l2@gmail.com (2018)</h4></div>
 </div>)
