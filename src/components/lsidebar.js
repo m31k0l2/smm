3f54411eb5c7a79322e06d088a977f66ff9fc0c7
m31k0l2 <m31k0l2@gmail.com>
2018-10-18 08:45:34 +0300

    v0

--- src/components/lsidebar.js
+++ src/components/lsidebar.js
@@ -11,12 +11,12 @@ export default class LSideBar extends React.Component {
         const styleName = this.state.isToggleOn ? '' : 'hideSidebar'
         return (
         <div className={[styles.sidebar, styles.transform, styles[styleName]].join(' ')}>
-            <div className={[styles.button, styles.hide].join(' ')}><a href="#" onClick={this.handleClick}><i>{this.state.isToggleOn ? <FaAngleLeft/> : <FaAngleRight/>}</i><span className={styles.transform}>Свернуть</span></a></div>
-            <div className={styles.item}><a href="#"><i><FaRegAddressCard/></i><span className={styles.transform}>ih8unem@gmail.com</span></a></div>
-            <div className={[styles.item, styles.active].join(' ')}><a href="#"><i><FaEnvelopeSquare/></i><span className={styles.transform}>Посты</span></a></div>
-            <div className={styles.item}><a href="#"><i><FaAddressBook/></i><span className={styles.transform}>Аккаунты</span></a></div>
+            <div className={[styles.button, styles.hide].join(' ')}><a className={styles.hideRef} href="#" onClick={this.handleClick}><i>{this.state.isToggleOn ? <FaAngleLeft/> : <FaAngleRight/>}</i><span className={styles.transform}>Свернуть</span></a></div>
+            <div data-descr="Настройки" className={styles.item}><a href="#"><i><FaRegAddressCard/></i><span className={styles.transform}>ih8unem@gmail.com</span></a></div>
+            <div data-descr="Посты" className={[styles.item, styles.active].join(' ')}><a href="#"><i><FaEnvelopeSquare/></i><span className={styles.transform}>Посты</span></a></div>
+            <div data-descr="Аккаунты" className={styles.item}><a href="#"><i><FaAddressBook/></i><span className={styles.transform}>Аккаунты</span></a></div>
             <div className={styles.empty}></div>
-            <div className={[styles.button, styles.exit].join(' ')}><a href="#"><i><FaSignOutAlt/></i><span className={styles.transform}>Выйти</span></a></div>
+            <div className={[styles.button, styles.exit].join(' ')}><a className={styles.exitRef} href="#"><i><FaSignOutAlt/></i><span className={styles.transform}>Выйти</span></a></div>
         </div>
         );
     }
